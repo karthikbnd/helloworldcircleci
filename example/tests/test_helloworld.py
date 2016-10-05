8a96836f687c7e4b972894b3107b20d06678562a
Karthik Bandi <karthikbandi@karthiks-air.replynet.prv>
2016-10-05 17:29:24 +0200

    aaded print

--- example/tests/test_helloworld.py
+++ example/tests/test_helloworld.py
@@ -1,9 +1,2 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
 print('Hello, world!')
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
+print ("Hello World!")
