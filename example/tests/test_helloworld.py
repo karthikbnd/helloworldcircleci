99a32d0b67521475ce02298bbaea32cf9b790833
Karthik Bandi <karthikbandi@karthiks-air.replynet.prv>
2016-10-05 17:22:56 +0200

    added helloworld

--- example/tests/test_helloworld.py
+++ example/tests/test_helloworld.py
@@ -1,1 +1,9 @@
-echo Hello, world!
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+print('Hello, world!')
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
